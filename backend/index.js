4083f0202be61cef03704f2455e01c6366bec224
Riddhi2909 <upadhyayriddhi445@gmail.com>
2025-05-01 14:42:50 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,11 @@ const dotenv = require("dotenv");
 const Stripe = require("stripe");
 
 dotenv.config();
-
 const app = express();
-app.use(cors({ origin: 'https://family-flavors.vercel.app/', credentials: true }));
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://family-flavors.vercel.app'],
+  credentials: true
+}));
 
 app.use(express.json({ limit: "10mb" }));
 
