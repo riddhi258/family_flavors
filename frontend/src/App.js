b17c32d4a270aed0bc40da3f4bc0eb3ad5b7f68c
Riddhi2909 <upadhyayriddhi445@gmail.com>
2025-05-05 11:18:46 +0530

    add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,24 +5,35 @@ import toast, { Toaster } from "react-hot-toast";
 import { useEffect } from "react";
 import { setDataProduct } from "./redux/productSlide";
 import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
 
 function App() {
-  const dispatch = useDispatch()
-  const productData = useSelector((state) => state.product)
+  const dispatch = useDispatch();
+  const productData = useSelector((state) => state.product);
 
   useEffect(() => {
-    (async () => {
-      const res = await fetch(`${process.env.REACT_APP_SERVER_DOMAIN}/product`);
-      const resData = await res.json();
-      dispatch(setDataProduct(resData));
-      console.log(resData);
-    })();  // <-- Fixed here: called the async function immediately
-  }, []);
+    const fetchProducts = async () => {
+      try {
+        const res = await axios.get(
+          `${process.env.REACT_APP_SERVER_DOMAIN}/product`
+          // OR use hardcoded URL:
+          // 'https://family-flavors-3bso.vercel.app/product'
+        );
+        dispatch(setDataProduct(res.data));
+        console.log(res.data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+        toast.error("Failed to load products");
+      }
+    };
 
-  console.log(productData)
+    fetchProducts();
+  }, [dispatch]);
+
+  console.log(productData);
 
   return (
-    <> 
+    <>
       <Toaster />
       <div>
         <Header />
