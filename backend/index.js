d08e2c7493ab1f4e5e71a81835f41f09139b30e1
Riddhi2909 <upadhyayriddhi445@gmail.com>
2025-05-05 11:01:51 +0530

    add

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,7 @@ app.use(cors({
 }));
 
 app.use(express.json({ limit: "10mb" }));
+mongoose.connect('mongodb+srv://riddhi:Y6i0eio9gA4oRnEx@riddhi.rseartm.mongodb.net/familyflavors?')
 
 const PORT = process.env.PORT || 3001;
 
